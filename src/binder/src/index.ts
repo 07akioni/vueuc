f065b8122e4015f0d97e81b5a1bd61247a6b6cdc
07akioni <07akioni2@gmail.com>
2021-01-14 18:49:10 +0800

    chore: export follower placement type

--- src/binder/src/index.ts
+++ src/binder/src/index.ts
@@ -1,3 +1,4 @@
 export { default as Binder } from './Binder'
 export { default as Target } from './Target'
 export { default as Follower } from './Follower'
+export { Placement as FollowerPlacement } from './interface'
