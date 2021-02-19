a9bf72b1f367711b9b1031432fd6311123faea50
07akioni <07akioni2@gmail.com>
2021-02-19 22:00:10 +0800

    fix(x-scroll): prevent scroll condition

--- src/x-scroll/src/index.ts
+++ src/x-scroll/src/index.ts
@@ -19,7 +19,7 @@ export default defineComponent({
   setup (props, { slots }) {
     function handleWheel (e: WheelEvent): void {
       const preventYWheel =
-        (e.target as HTMLElement).offsetWidth >
+        (e.target as HTMLElement).offsetWidth <
         (e.target as HTMLElement).scrollWidth
       if (!preventYWheel || e.deltaY === 0) return;
       (e.currentTarget as HTMLElement).scrollLeft += e.deltaY + e.deltaX
