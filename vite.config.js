0e2d5d6bf9af4561edc293721b8b9221ca4d089a
07akioni <07akioni2@gmail.com>
2021-03-03 16:03:01 +0800

    fix: site build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ const path = require('path')
  */
 module.exports = {
   root: 'src',
+  define: {
+    __VUE_PROD_DEVTOOLS__: false
+  },
   build: {
     outDir: path.resolve(__dirname, 'site')
   }
