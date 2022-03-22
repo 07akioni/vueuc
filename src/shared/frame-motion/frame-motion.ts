c6d47e96c69c8d7695569e1b89f44a2427332b2e
07akioni <07akioni2@gmail.com>
2022-03-23 03:27:44 +0800

    feat: remove redundant code

--- src/shared/frame-motion/frame-motion.ts
+++ src/shared/frame-motion/frame-motion.ts
@@ -7,110 +7,72 @@ import {
 } from './utils'
 import { easings } from './easing'
 import type {
-  FrameMotionScheduler,
+  Easing,
   FrameMotionController,
   FrameMotionOptions
 } from './interface'
 
-type ScheduleRequireKey =
-  | 'getTimeStamp'
-  | 'requestFrame'
-  | 'cancelFrame'
-  | 'process'
-type Schedule = Omit<FrameMotionScheduler, ScheduleRequireKey> &
-Required<Pick<FrameMotionScheduler, ScheduleRequireKey>>
-
 export function createFrameMotion ({
   duration = 500,
   easing = easings.linear,
-  onPlay,
-  onStop,
   onComplete,
   onUpdate
 }: FrameMotionOptions): FrameMotionController {
-  const schedule: Schedule = {
-    getTimeStamp,
-    requestFrame,
-    cancelFrame,
-    process: (timeElapsed, duration, easing) => {
-      return easing(calculateProgress(timeElapsed, duration))
-    }
+  let completed = false
+  let timeElapsed = 0
+  let startTime: number | null = null
+  let frameId: number | null = null
+
+  const getProcess = (
+    timeElapsed: number,
+    duration: number,
+    easing: Easing
+  ): number => {
+    return easing(calculateProgress(timeElapsed, duration))
   }
 
-  let startTime: number | null = null
-  let timeElapsed = 0
-
-  let frameId: any = null
-
-  let stopping = false
-  let completed = false
-
   const play = (): void => {
-    // stop to play
-    const isContinue = startTime !== null
-    if (completed || (isContinue && !stopping)) {
+    if (completed) {
       return
     }
 
-    const currentTime = (startTime = schedule.getTimeStamp())
+    const currentTime = (startTime = getTimeStamp())
 
     const doWork = (): void => {
-      schedule.onPlay?.(currentTime)
-      onPlay?.(currentTime)
-
-      schedule.requestFrame(() => work(currentTime))
+      requestFrame(() => work(currentTime))
     }
 
-    if (isContinue) {
-      stopping = false
-      // When restarting, we need to make up for the time that's gone by.
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      startTime -= timeElapsed
-    }
     doWork()
   }
 
   const work = (currentTime: number): void => {
-    if (stopping || completed) {
+    if (completed) {
       return
     }
     if (startTime === null) {
-      startTime = currentTime
+      throw new Error('[vueuc/frame-motion]: startTime is null')
     }
-
     timeElapsed = currentTime - startTime
     const progress = normalizeProgress(
-      schedule.process(timeElapsed, duration, easing)
+      getProcess(timeElapsed, duration, easing)
     )
-
-    const shouldCallUpdate =
-      schedule.shouldCallUpdate?.(currentTime, timeElapsed) ?? true
-    if (shouldCallUpdate) {
-      onUpdate?.(progress, timeElapsed)
-    }
-
+    onUpdate(progress, timeElapsed)
     if (progress >= 1 || timeElapsed >= duration) {
       completed = true
-      schedule.onComplete?.(currentTime)
-      onComplete?.(currentTime)
+      onComplete(currentTime)
       return
     }
-
-    frameId = schedule.requestFrame(work)
+    frameId = requestFrame(work)
   }
 
   const stop = (): void => {
-    if (!stopping && !completed) {
-      const stopTime = schedule.getTimeStamp()
-      stopping = true
-
-      if (frameId !== null) {
-        schedule.cancelFrame(frameId)
-        frameId = null
-      }
-
-      schedule.onStop?.(stopTime)
-      onStop?.(stopTime)
+    if (!completed) {
+      // abort the motion
+      completed = true
+    }
+    if (frameId !== null) {
+      cancelFrame(frameId)
+      frameId = null
     }
   }
 
--- src/shared/frame-motion/interface.ts
+++ src/shared/frame-motion/interface.ts
@@ -1,10 +1,8 @@
 export type Easing = (progress: number) => number
 
 export interface FrameMotionHooks {
-  onPlay?: (time: number) => void
-  onStop?: (time: number) => void
-  onUpdate?: (progress: number, timeElapsed: number) => void
-  onComplete?: (time: number) => void
+  onUpdate: (progress: number, timeElapsed: number) => void
+  onComplete: (time: number) => void
 }
 
 export interface FrameMotionOptions extends FrameMotionHooks {
@@ -18,17 +16,17 @@ export interface FrameMotionOptions extends FrameMotionHooks {
   easing?: Easing
 }
 
-export interface FrameMotionScheduler
-  extends Omit<FrameMotionHooks, 'onUpdate'> {
-  // time scheduler
-  getTimeStamp?: () => number
-  requestFrame?: (callback: (currentTime: number) => void) => any
-  cancelFrame?: (handle: any) => void
+// export interface FrameMotionScheduler
+//   extends Omit<FrameMotionHooks, 'onUpdate'> {
+//   // time scheduler
+//   getTimeStamp?: () => number
+//   requestFrame?: (callback: (currentTime: number) => void) => any
+//   cancelFrame?: (handle: any) => void
 
-  shouldCallUpdate?: (currentTime: number, timeElapsed: number) => boolean
-  onUpdate?: (time: number) => void
-  process?: (timeElapsed: number, duration: number, easing: Easing) => number
-}
+//   shouldCallUpdate?: (currentTime: number, timeElapsed: number) => boolean
+//   onUpdate?: (time: number) => void
+//   process?: (timeElapsed: number, duration: number, easing: Easing) => number
+// }
 
 export interface FrameMotionController {
   play: () => void
