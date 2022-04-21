77c62d44f24bf0f3ffef6e3b307efc0ec4ee2831
07akioni <07akioni2@gmail.com>
2022-04-22 02:01:09 +0800

    fix: resize observer manager export

--- src/resize-observer/src/index.ts
+++ src/resize-observer/src/index.ts
@@ -1,3 +1,3 @@
 export { default } from './VResizeObserver'
 export type { VResizeObserverOnResize } from './VResizeObserver'
-export { default as resizeObserverManager } from './VResizeObserver'
+export { default as resizeObserverManager } from './delegate'
