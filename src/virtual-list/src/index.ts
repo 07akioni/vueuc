6455c225b9a586cabb56940beefc291baf43ff27
07akioni <07akioni2@gmail.com>
2023-12-21 01:18:43 +0800

    feat(virtual-list): export some types

--- src/virtual-list/src/index.ts
+++ src/virtual-list/src/index.ts
@@ -1,2 +1,9 @@
 export { default } from './VirtualList'
-export type { VirtualListInst } from './VirtualList'
+export type {
+  VirtualListInst,
+  ScrollTo as VirtualListScrollTo
+} from './VirtualList'
+export type {
+  VScrollToOptions as VirtualListScrollOptions,
+  ItemData as VirtualListItemData
+} from './type'
